a350148fc217e4b27893fa7b0a1e38b229446727
Sherif Ahmed <sherif17.ahmed@gmail.com>
2020-11-07 19:42:01 +0200

    Simple login screen

---
+++ app/src/main/java/com/example/sheri/login_screen/Screen1.java
@@ -0,0 +1,13 @@
+package com.example.sheri.login_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+    }
+}
